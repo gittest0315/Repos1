4be457f355e268cffe112b3fdf252da230f4f892
Toshiyuki Okubo <v-tookubo@microsoft.com>
2021-03-15 14:13:06 +0900

    0315

---
+++ TestAquireUserToken/TestAquireUserToken/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace TestAquireUserToken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ResultText = new System.Windows.Forms.Label();
+            this.TokenInfoText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultText
+            // 
+            this.ResultText.AutoSize = true;
+            this.ResultText.Location = new System.Drawing.Point(41, 38);
+            this.ResultText.Name = "ResultText";
+            this.ResultText.Size = new System.Drawing.Size(61, 12);
+            this.ResultText.TabIndex = 1;
+            this.ResultText.Text = "ResultText";
+            // 
+            // TokenInfoText
+            // 
+            this.TokenInfoText.AutoSize = true;
+            this.TokenInfoText.Location = new System.Drawing.Point(293, 38);
+            this.TokenInfoText.Name = "TokenInfoText";
+            this.TokenInfoText.Size = new System.Drawing.Size(78, 12);
+            this.TokenInfoText.TabIndex = 2;
+            this.TokenInfoText.Text = "TokenInfoText";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 175);
+            this.Controls.Add(this.TokenInfoText);
+            this.Controls.Add(this.ResultText);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label ResultText;
+        private System.Windows.Forms.Label TokenInfoText;
+    }
+}
+
---
+++ TestAquireUserToken/TestAquireUserToken/Form1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAquireUserToken
+{
+    public partial class Form1 : Form
+    {
+        //Set the API Endpoint to Graph 'me' endpoint
+        //string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        //Set the scope for API call to user.read
+        string[] scopes = new string[] { "user.read" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Call AcquireToken - to acquire a token requiring user to sign-in
+        /// </summary>
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Microsoft.Identity.Client.AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+            ResultText.Text = string.Empty;
+            TokenInfoText.Text = string.Empty;
+
+            var accounts = await app.GetAccountsAsync();
+            var firstAccount = accounts.FirstOrDefault();
+
+            try
+            {
+                authResult = await app.AcquireTokenSilent(scopes, firstAccount)
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilent.
+                // This indicates you need to call AcquireTokenInteractive to acquire a token
+                System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    authResult = await app.AcquireTokenInteractive(scopes)
+                        .WithAccount(accounts.FirstOrDefault())
+                        .WithPrompt(Prompt.SelectAccount)
+                        .ExecuteAsync();
+                }
+                catch (MsalException msalex)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+                return;
+            }
+
+            //if (authResult != null)
+            //{
+            //    ResultText.Text = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
+            //    DisplayBasicTokenInfo(authResult);
+            //    this.SignOutButton.Visibility = Visibility.Visible;
+            // }
+        }
+    }
+    public partial class App
+    {
+        static App()
+        {
+            _clientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                .WithDefaultRedirectUri()
+                .Build();
+        }
+
+        // Below are the clientId (Application Id) of your app registration and the tenant information.
+        // You have to replace:
+        // - the content of ClientID with the Application Id for your app registration
+        // - the content of Tenant by the information about the accounts allowed to sign-in in your application:
+        //   - For Work or School account in your org, use your tenant ID, or domain
+        //   - for any Work or School accounts, use `organizations`
+        //   - for any Work or School accounts, or Microsoft personal account, use `common`
+        //   - for Microsoft Personal account, use consumers
+        private static string ClientId = "f9f85734-81b5-4e8f-8241-232f9df92a5b";
+
+        private static string Tenant = "common";
+
+        private static IPublicClientApplication _clientApp;
+
+        public static IPublicClientApplication PublicClientApp { get { return _clientApp; } }
+    }
+}
